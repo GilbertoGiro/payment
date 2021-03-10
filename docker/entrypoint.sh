376a3aae3b2a933c11b5a7f191b039a88605319c
Gilberto Resende <gilberto.giro.resende@gmail.com>
2021-03-10 20:59:44 -0300

    [Development] - Docker configuration (Database structure: MySQL)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+echo "===> Wait until mysql is ready"
+/wait
+
 # Copy environment file
 echo "===> Copying environment file"
 cp /opt/payment/.env.example /opt/payment/.env
@@ -7,6 +10,10 @@ cp /opt/payment/.env.example /opt/payment/.env
 # Run composer install
 echo "===> Running composer"
 composer install --no-interaction
+
+# Run migrations
+echo "===> Running migrations"
+php artisan migrate
 
 # Run supervisor
 echo "===> Running supervisor"
